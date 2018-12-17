73cdcf3f731052c8ec9cce33183a82d928f7d3a8
eugenia <muchnikova@gmail.com>
2018-12-17 15:16:36 +0000

    first commit

--- src/java/com/taxbands/taxbands/ProgressiveTaxCalculator.java
+++ src/java/com/taxbands/taxbands/ProgressiveTaxCalculator.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-public class ProgressiveTaxCalculator {
+public class ProgressiveTaxCalculator{
 
     private final TaxBandList taxBandList;
 
